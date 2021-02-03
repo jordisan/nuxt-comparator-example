1d90d7a3333e7dc8f77c31a629e9196708300798
jordisan <ramsys@gmail.com>
2021-02-03 17:42:32 +0100

    Fix text

--- test/Antagonist.spec.ts
+++ test/Antagonist.spec.ts
@@ -1,13 +1,15 @@
 import { mount } from '@vue/test-utils'
-import Antagonist from '@/components/Antagonist.vue'
+import Antagonist from '@/components/antagonists/Antagonist.vue'
 
 describe('Antagonist', () => {
-  test('renders an antagonist', async () => {
-    const wrapper = mount(Antagonist, {propsData: {
-      content: {
-        title: 'antagonist test'
+  test('renders an antagonist', () => {
+    const wrapper = mount(Antagonist, {
+      propsData: {
+        content: {
+          title: 'antagonist test'
+        }
       }
-    }})
+    })
     expect(wrapper.find('h1').exists()).toBeTruthy()
     console.log('h1: ' + wrapper.find('h1')?.text())
   })
