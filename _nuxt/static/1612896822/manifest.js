7af044993400a6526da1eadd8f0f6e7c630993a8
jordisan <jordisan@users.noreply.github.com>
2021-02-09 18:53:43 +0000

    deploy: 8e9234de376625f52fea228c9675c6b0a5ebff22

---
+++ _nuxt/static/1612896822/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fabout","\u002Fantagonists","\u002Fblog","\u002Fantagonists\u002Fantagonist1","\u002Fantagonists\u002Fantagonist2","\u002Fantagonists\u002Fantagonist3","\u002Fblog\u002Fblog-entry-2","\u002Fblog\u002Fblog-entry-1"]})
