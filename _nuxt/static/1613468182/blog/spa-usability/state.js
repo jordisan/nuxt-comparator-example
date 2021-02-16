87ce95210a3366620dc035697cfe4048d2d82ef0
jordisan <jordisan@users.noreply.github.com>
2021-02-16 09:37:50 +0000

    deploy: 86c02582167899904cbed470c55ccd8e937b0770

--- _nuxt/static/1613468182/blog/spa-usability/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1613468182",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fspa-usability",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f25415e7"}}}}(null));
--- _nuxt/static/1613468182/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1613468182",layout:"home",error:a,serverRendered:true,routePath:b,config:{app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f25415e7"}}}}(null,"\u002F"));
