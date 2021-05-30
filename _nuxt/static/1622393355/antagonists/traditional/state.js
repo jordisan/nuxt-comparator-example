407f5ba6f15256231ad0f83c516fd8479c79a9ed
jordisan <jordisan@users.noreply.github.com>
2021-05-30 16:49:16 +0000

    deploy: 6c82ac459a98c9cd93a580182ce1a0fda8bee5d3

---
+++ _nuxt/static/1622393355/antagonists/traditional/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1622393355",layout:"default",error:a,serverRendered:true,routePath:"\u002Fantagonists\u002Ftraditional",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ff32576d"}}}}(null));
