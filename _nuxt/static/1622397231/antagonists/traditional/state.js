63dce8a416679b1eb6085ea94598710242a8edff
jordisan <jordisan@users.noreply.github.com>
2021-05-30 17:53:52 +0000

    deploy: bc3d5b45341ae5e35c0a281a3b6b3df223050a24

---
+++ _nuxt/static/1622397231/antagonists/traditional/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1622397231",layout:"default",error:a,serverRendered:true,routePath:"\u002Fantagonists\u002Ftraditional",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"51054be0"}}}}(null));
---
+++ _nuxt/static/1622397231/blog/static-generation/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1622397231",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fstatic-generation",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"51054be0"}}}}(null));
