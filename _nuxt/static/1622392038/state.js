699fb27869f959377ce33193e87a4f4de63de763
jordisan <jordisan@users.noreply.github.com>
2021-05-30 16:31:59 +0000

    deploy: 084bc3f09aded4e057bfd817b895059b3ceaef2f

--- _nuxt/static/1622392038/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1622392038",layout:"home",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"295524eb"}}}}(null,"\u002F"));
---
+++ _nuxt/static/1622392317/antagonists/traditional/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1622392317",layout:"default",error:a,serverRendered:true,routePath:"\u002Fantagonists\u002Ftraditional",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6d337b53"}}}}(null));
