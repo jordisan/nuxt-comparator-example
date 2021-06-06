b00160d0c97c6a177d79ada8983cd2bcc0402647
jordisan <jordisan@users.noreply.github.com>
2021-06-06 17:21:14 +0000

    deploy: 41684230c6f496e6cbe939e6e44128297528f871

---
+++ _nuxt/static/1623000073/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Fantagonists","\u002F","\u002Fblog","\u002Fabout","\u002Fantagonists\u002Ftraditional","\u002Fantagonists\u002Fspa","\u002Fblog\u002Fspa-usability","\u002Fblog\u002Fstatic-generation"]})
---
+++ _nuxt/static/1623000073/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1623000073",layout:"home",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c07ad13a"}}}}(null,"\u002F"));
