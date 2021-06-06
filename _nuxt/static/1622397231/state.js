2f222550cb2a942c2653ab2df043a52e28d240ff
jordisan <jordisan@users.noreply.github.com>
2021-06-06 17:17:43 +0000

    deploy: 6d369da624ebdb9c73495ae1e701689b1271677c

--- _nuxt/static/1622397231/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1622397231",layout:"home",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"51054be0"}}}}(null,"\u002F"));
