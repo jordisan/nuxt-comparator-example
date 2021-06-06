a482293589e02f3e04bfec4cf92e3d9e89bddf6c
jordisan <jordisan@users.noreply.github.com>
2021-06-06 17:31:16 +0000

    deploy: 6731204c570ce251075cf0c16d499762fd90a7a8

--- _nuxt/static/1623000460/antagonists/spa/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1623000460",layout:"default",error:a,serverRendered:true,routePath:"\u002Fantagonists\u002Fspa",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"1155b819"}}}}(null));
